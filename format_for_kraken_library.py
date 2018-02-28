b4287f69fa2a78e31e0a4f645b4f263a421a4aac
LCrossman <lisa.crossman@gmail.com>
2018-02-28 11:05:15 +0000

    update_kraken_script

--- format_for_kraken_library.py
+++ format_for_kraken_library.py
@@ -3,29 +3,31 @@
 
 #requires biopython
 
+import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 
 try:
-    with open(sys.argv[1], 'r') as handle:
-        records = list(SeqIO.parse(handle, "fasta"))
+    handle = open(sys.argv[1], 'r')
 except:
     print("Usage: format_for_kraken_library.py fastafile")
 
 
+records = list(SeqIO.parse(handle, "fasta"))
 sequences = []
 i = 0
 
 
 for rec in records:
+    try:
        short = rec.id
-       newid = "{}|kraken:taxid|{}".format(id,i)
+       newid = "{}|kraken:taxid|{}".format(rec.id,i)
        new_rec = SeqRecord(rec.seq, id=newid, description="")
        sequences.append(new_rec)
        i+=1
-    else:
+    except:
         print("formatting issue", rec.id)
 
 
